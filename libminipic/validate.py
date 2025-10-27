9c08a92640bd2f94fb16a36106d5e00993cc74b4
Paul Zehner <paul.zehner@cea.fr>
2025-10-27 14:56:05 +0100

    Allow mini-validate to autodetect setup

--- libminipic/validate.py
+++ libminipic/validate.py
@@ -2,19 +2,42 @@
 
 import importlib
 import os
+import re
 import sys
 from argparse import ArgumentParser
+from pathlib import Path
+
+CMAKE_CACHE_FILENAME = "CMakeCache.txt"
 
 
-def validate_setup(path, setup, threshold):
+def detect_setup(path: Path) -> str:
+    cmake_cache_file = path / CMAKE_CACHE_FILENAME
+    if not cmake_cache_file.exists():
+        raise RuntimeError(f"Cannot find {cmake_cache_file}")
+
+    cmake_cache_content = cmake_cache_file.read_text()
+    matcher = re.findall(r"MINIPIC_SETUP:STRING=(.*)", cmake_cache_content)
+
+    if not matcher:
+        raise RuntimeError(f"Cannot find setup in {cmake_cache_file}")
+
+    # return the first element, as we know there is at least one
+    return matcher[0].strip()
+
+
+def validate_setup(path, setup=None, threshold=1e-10):
+    if not setup:
+        setup = detect_setup(path)
+        print(f"Autodetected setup: {setup}")
+
     module = importlib.import_module(f"libminipic.validation.{setup}", None)
 
-    if path:
-        os.chdir(path)
+    os.chdir(path)
 
     if not os.path.isdir("diags"):
-        print("Directory diags should be present where you run this script")
-        sys.exit(1)
+        raise RuntimeError(
+            "Directory diags should be present where you run this script"
+        )
 
     module.validate(threshold)
 
@@ -28,8 +51,16 @@ def validate_setup(path, setup, threshold):
 def validate():
     parser = ArgumentParser(description="Run a validation script")
 
-    parser.add_argument("setup", help="name of the setup to validate")
-    parser.add_argument("--path", help="path to the execution directory")
+    parser.add_argument(
+        "-s", "--setup", help="name of the setup to validate (default to autodetect)"
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        help="path to the execution directory (default to current working directory)",
+        type=Path,
+        default=Path.cwd(),
+    )
     parser.add_argument(
         "--threshold", help="threshold for the validation", default=1e-10, type=float
     )
